9f21c43aa561222e3d6d867f184c93b39b71b3b3
vinayvedula <vinaykumar_vedula@yahoo.co.in>
2023-04-03 17:08:11 -0500

    "[Commit 1: Spring Boot Initializer for IC framework]"

---
+++ src/main/java/com/vinay/InquireCustomer/InquireCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.InquireCustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquireCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquireCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinay/InquireCustomer/InquireCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.InquireCustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquireCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
