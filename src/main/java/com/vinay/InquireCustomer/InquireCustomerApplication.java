b08ecae272b7be30bf989be8a99ead72840650a4
vinayvedula <vinaykumar_vedula@yahoo.co.in>
2023-04-04 00:41:09 -0500

    "[Commit 2: Untested Source Code includes Controller, Model, Repository, Exception]"

--- src/main/java/com/vinay/InquireCustomer/InquireCustomerApplication.java
+++ src/main/java/com/vinay/InquireCustomer/InquireCustomerApplication.java
@@ -2,8 +2,10 @@ package com.vinay.InquireCustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InquireCustomerApplication {
 
 	public static void main(String[] args) {
